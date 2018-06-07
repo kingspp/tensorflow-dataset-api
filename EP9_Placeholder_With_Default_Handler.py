8d03e7b5b6e6f22ebd2d30deb1ad688aac87fc3a
Prathyush SP <kingspprathyush@gmail.com>
2018-06-07 15:15:33 +0530

    added tracing

--- EP9_Placeholder_With_Default_Handler.py
+++ EP9_Placeholder_With_Default_Handler.py
@@ -15,11 +15,12 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import time
+from tensorflow.python.client import timeline
 
 start = time.time()
 
 # Global Variables
-EPOCH = 10
+EPOCH = 1
 BATCH_SIZE = 32
 DISPLAY_STEP = 1
 
@@ -52,6 +53,7 @@ features, labels = iterator.get_next()
 features_placeholder = tf.placeholder_with_default(features, [None, mnist.train.images.shape[-1]])
 labels_placeholder = tf.placeholder_with_default(labels, [None, mnist.train.labels.shape[-1]])
 
+
 # Deeplearning Model
 def nn_model(features, labels):
     bn = tf.layers.batch_normalization(features)
@@ -68,17 +70,19 @@ def nn_model(features, labels):
 training_op, loss_op = nn_model(features=features_placeholder, labels=labels_placeholder)
 init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
+options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+run_metadata = tf.RunMetadata()
 
 # Training without Feed Dict
 with tf.train.MonitoredTrainingSession() as sess:
     sess.run(init_op)
     training_handle = sess.run(training_iterator.string_handle())
     validation_handle = sess.run(validation_iterator.string_handle())
+
     batch_id, epoch_id, total_batches, avg_cost = 0, 0, int(mnist.train.num_examples / BATCH_SIZE), 0
     while not sess.should_stop():
-        # print(sess.run(features).shape)
-        # print(sess.run(labels).shape)
-        _, c = sess.run([training_op, loss_op], feed_dict={handle:training_handle})
+        _, c = sess.run([training_op, loss_op], feed_dict={handle: training_handle}, options=options,
+                        run_metadata=run_metadata)
         avg_cost += c / total_batches
         if batch_id == total_batches:
             if epoch_id % DISPLAY_STEP == 0:
@@ -88,26 +92,29 @@ with tf.train.MonitoredTrainingSession() as sess:
         batch_id += 1
     print("Optimization Finished!")
 
-print('Total Time Elapsed: {} secs'.format(time.time()-start))
-
-# Training with Feed Dict
-with tf.Session() as sess:
-    sess.run(init_op)
-    total_batches = int(mnist.train.num_examples / BATCH_SIZE)
-    for epoch in range(EPOCH):
-        avg_cost = 0.0
-        # Loop over all batches
-        for i in range(total_batches):
-            batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
-            _, c = sess.run([training_op, loss_op], feed_dict={features_placeholder: batch_x,
-                                                               labels_placeholder: batch_y})
-            # Compute average loss
-            avg_cost += c / total_batches
-        # Display logs per epoch step
-        if epoch % DISPLAY_STEP == 0:
-            print("Epoch:", '%04d' % (epoch + 1), "cost={:.9f}".format(avg_cost))
-    print("Optimization Finished!")
-
-
-
 print('Total Time Elapsed: {} secs'.format(time.time() - start))
+
+# # Training with Feed Dict
+# with tf.Session() as sess:
+#     sess.run(init_op)
+#     total_batches = int(mnist.train.num_examples / BATCH_SIZE)
+#     for epoch in range(EPOCH):
+#         avg_cost = 0.0
+#         # Loop over all batches
+#         for i in range(total_batches):
+#             batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
+#             _, c = sess.run([training_op, loss_op], feed_dict={features_placeholder: batch_x,
+#                                                                labels_placeholder: batch_y},
+#                             options=options, run_metadata=run_metadata)
+#             # Compute average loss
+#             avg_cost += c / total_batches
+#         # Display logs per epoch step
+#         if epoch % DISPLAY_STEP == 0:
+#             print("Epoch:", '%04d' % (epoch + 1), "cost={:.9f}".format(avg_cost))
+#     print("Optimization Finished!")
+# print('Total Time Elapsed: {} secs'.format(time.time() - start))
+
+fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+chrome_trace = fetched_timeline.generate_chrome_trace_format()
+with open('timeline_generator-feed_dict.json', 'w') as f:
+    f.write(chrome_trace)
