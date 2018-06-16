313921d231646f02db6abf844295a830c1a3ba38
Prathyush SP <kingspprathyush@gmail.com>
2018-06-16 18:09:07 +0530

    updated scripts

--- benchmark_scripts/EP3_Feedable_Dataset.py
+++ benchmark_scripts/EP3_Feedable_Dataset.py
@@ -15,16 +15,8 @@ import json
 import tensorflow as tf
 from benchmark_scripts.common_config import nn_model, config_proto, EPOCH, BATCH_SIZE, DISPLAY_STEP, get_butil, mnist
 import time
-from threading import Thread
 
 butil = get_butil(__file__)
-
-THREADS_ENABLED = True
-
-
-def saver_fn(i, session: tf.Session, saver: tf.train.Saver):
-    print('Saving Session {}. . .'.format(i))
-    saver.save(session, '/tmp/new_save', global_step=i)
 
 
 @butil.monitor
@@ -49,33 +41,35 @@ def main():
     # Create elements from iterator
     training_op, loss_op = nn_model(features=features, labels=labels)
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-    threads = []
-    saver = tf.train.Saver()
-    start = time.time()
-    with tf.Session() as sess:
-        sess.run(init_op)
-        batch_id, total_batches, avg_cost = 0, int(mnist.train.num_examples / BATCH_SIZE), 0
-        for i in range(EPOCH):
-            sess.run(iterator.initializer)
-            try:
-                while True:
-                    _, c = sess.run([training_op, loss_op])
-                    avg_cost += c / total_batches
-                    batch_id += 1
-            except tf.errors.OutOfRangeError:
-                if i % DISPLAY_STEP == 0:
-                    print("Epoch:", '%04d' % (i + 1), "cost={:.9f}".format(avg_cost))
-                    if THREADS_ENABLED:
-                        threads.append(Thread(target=saver_fn, args=(i, sess, saver)))
-                        threads[-1].start()
-                    else:
-                        print('Saving Session {}. . .'.format(i))
-                        saver.save(sess, '/tmp/new_save', global_step=i)
-                batch_id, avg_cost, cost = 0, 0, []
 
-        print("Optimization Finished!")
-        if THREADS_ENABLED:
-            [i.join() for i in threads]
+    start = time.time()
+    with tf.train.MonitoredTrainingSession(config=config_proto) as sess:
+        sess.run(init_op)
+        sess.run(iterator.initializer, feed_dict={features_placeholder: mnist.train.images,
+                                                  labels_placeholder: mnist.train.labels})
+        batch_id, epoch_id, total_batches, avg_cost = 0, 0, int(mnist.train.num_examples / BATCH_SIZE), 0
+        try:
+            while True:
+                _, c = sess.run([training_op, loss_op])
+                avg_cost += c / total_batches
+                if batch_id == total_batches:
+                    if epoch_id % DISPLAY_STEP == 0:
+                        print("Epoch:", '%04d' % (epoch_id + 1), "cost={:.9f}".format(avg_cost))
+                    batch_id, avg_cost, cost = 0, 0, []
+                    epoch_id += 1
+                batch_id += 1
+        except tf.errors.OutOfRangeError:
+            print("Optimization Finished!")
+
+        sess.run(iterator.initializer, feed_dict={features_placeholder: mnist.test.images,
+                                                  labels_placeholder: mnist.test.labels})
+        total_batches, avg_cost = int(mnist.test.num_examples / BATCH_SIZE), 0.0
+        try:
+            while True:
+                _, c = sess.run([training_op, loss_op])
+                avg_cost += c / total_batches
+        except tf.errors.OutOfRangeError:
+            print("Validation :", "cost={:.9f}".format(avg_cost))
 
     print('Total Time Elapsed: {} secs'.format(time.time() - start))
     json.dump({'internal_time': time.time() - start}, open('/tmp/time.json', 'w'))
